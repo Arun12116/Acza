e005e680dc9ec28bae8549aa8bb00bf89249d9d8
Arun <yarunkumar136@gmail.com>
2023-08-02 16:25:27 +0530

    response

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -13,15 +13,15 @@ const Home = () => {
 
           <source src={hi} />
         </video>
-          <div className="heading" style={{ position: "absolute", top: "50%", left: "0", right: "0", textAlign: "center" }}>
-            <h1>Wlecome to Our Geeks-Store</h1>
-            <p style={{ padding: "0 10rem" }}>Lorem ipsum dolor, sit amet Eos aliquam officiis dolor?
-              ipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipit
-              Velit aspernatur suscipit provident laboriosam labore error delectus accusamus! Adipisci hic accusamus dicta maxime ullam, aut ipsam facere?</p>
-            <button>Expolore More</button>
+        <div className="heading" style={{ position: "absolute", top: "50%", left: "0", right: "0", textAlign: "center" }}>
+          <h1>Wlecome to Our Geeks-Store</h1>
+          <p style={{ padding: "0 10rem" }}>Lorem ipsum dolor, sit amet Eos aliquam officiis dolor?
+            ipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipitipsum dolor, sit amet Eos aliquam officiis dolor? Velit aspernatur suscipit
+            Velit aspernatur suscipit provident laboriosam labore error delectus accusamus! Adipisci hic accusamus dicta maxime ullam, aut ipsam facere?</p>
+          <button>Expolore More</button>
 
-          </div>
-      
+        </div>
+
 
 
 
